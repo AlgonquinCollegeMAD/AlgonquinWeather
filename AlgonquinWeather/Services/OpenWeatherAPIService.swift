7510076701e2516c9fa067897a1e1946cc3942a2
Vladimir Cezar <vladimircezar@icloud.com>
2023-11-07 14:42:30 -0500

    Create methods to retrieve and decode Forecast API data

--- AlgonquinWeather/Services/OpenWeatherAPIService.swift
+++ AlgonquinWeather/Services/OpenWeatherAPIService.swift
@@ -11,14 +11,34 @@ class OpenWeatherAPIService: ObservableObject {
     let urlString = "http://api.openweathermap.org/geo/1.0/direct?q=\(searchString)&limit=20&appid=df766d0efdfecd9cf618f52dc2c16a02"
     if let url = URL(string: urlString) {
       let (data, _) = try await URLSession.shared.data(from: url)
-      return decodeData(from: data)
+      return decodeLocationData(from: data)
     } else {
       throw WeatherError.invalidURL
     }
   }
   
+  func getForecast(location: Location) async throws -> Forecast? {
+    let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(location.lat)lon=\(location.lon)&dt=1200&appid=df766d0efdfecd9cf618f52dc2c16a02"
+    if let url = URL(string: urlString) {
+      let (data, _) = try await URLSession.shared.data(from: url)
+      return decodeForecastData(from: data)
+    } else {
+      throw WeatherError.invalidURL
+    }
+  }
   /** Private functions **/
-  private func decodeData(from json: Data) -> [Location] {
+  private func decodeForecastData(from json: Data) -> Forecast? {
+    do {
+      let decoder = JSONDecoder()
+      let data = try decoder.decode(Forecast.self, from: json)
+      return data
+    } catch {
+      print("Error decoding weather data: \(error)")
+      return nil
+    }
+  }
+  
+  private func decodeLocationData(from json: Data) -> [Location] {
     do {
       let decoder = JSONDecoder()
       let data = try decoder.decode([Location].self, from: json)

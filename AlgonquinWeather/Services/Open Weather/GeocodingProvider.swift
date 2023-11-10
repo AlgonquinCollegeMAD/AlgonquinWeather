4ea9f0e82112854a256068bf9912f6d3a1f1d69f
Vladimir Cezar <vladimircezar@icloud.com>
2023-11-09 19:02:18 -0500

    Refator Services to remove unecessary decode function

--- AlgonquinWeather/Services/Open Weather/GeocodingProvider.swift
+++ AlgonquinWeather/Services/Open Weather/GeocodingProvider.swift
@@ -6,20 +6,16 @@ extension OpenWeather {
       let urlString = "http://api.openweathermap.org/geo/1.0/direct?q=\(searchString)&limit=20&appid=df766d0efdfecd9cf618f52dc2c16a02"
       if let url = URL(string: urlString) {
         let (data, _) = try await URLSession.shared.data(from: url)
-        return decodeData(from: data)
+        do {
+          let decoder = JSONDecoder()
+          let locations = try decoder.decode([LocationModel].self, from: data)
+          return locations
+        } catch {
+          print("Error decoding weather data: \(error)")
+          return []
+        }
       } else {
         throw WeatherError.invalidURL
-      }
-    }
-    
-    private func decodeData(from json: Data) -> [LocationModel] {
-      do {
-        let decoder = JSONDecoder()
-        let data = try decoder.decode([LocationModel].self, from: json)
-        return data
-      } catch {
-        print("Error decoding weather data: \(error)")
-        return []
       }
     }
   }
--- AlgonquinWeather/Services/Open Weather/WeatherService.swift
+++ AlgonquinWeather/Services/Open Weather/WeatherService.swift
@@ -6,20 +6,16 @@ extension OpenWeather {
       let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(location.lat)&lon=\(location.lon)&units=metric&&dt=1200&appid=df766d0efdfecd9cf618f52dc2c16a02"
       if let url = URL(string: urlString) {
         let (data, _) = try await URLSession.shared.data(from: url)
-        return decodeData(from: data)
+        do {
+          let decoder = JSONDecoder()
+          let currentWeather = try decoder.decode(CurrentWeatherModel.self, from: data)
+          return currentWeather
+        } catch {
+          print("Error decoding weather data: \(error)")
+          return nil
+        }
       } else {
         throw WeatherError.invalidURL
-      }
-    }
-    
-    private func decodeData(from json: Data) -> CurrentWeatherModel? {
-      do {
-        let decoder = JSONDecoder()
-        let data = try decoder.decode(CurrentWeatherModel.self, from: json)
-        return data
-      } catch {
-        print("Error decoding weather data: \(error)")
-        return nil
       }
     }
   }
